69cb3d69c1580e2deb6dcdafa4f1b217be3a1a4c
kyungbin <thrudqls13@naver.com>
2025-01-30 03:09:36 +0900

    Material UI Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>      
-    </div>
+    <Typography variant='h2' component='h2' data-test='heading'>
+      Bookish      
+    </Typography>
   );
 }
 
 export default App;
-
