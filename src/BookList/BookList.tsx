baaaba2e6f129e2e36383e82e7d12fe669161259
kyungbin <thrudqls13@naver.com>
2025-02-12 19:24:33 +0900

    User Interface Refinement

--- src/BookList/BookList.tsx
+++ src/BookList/BookList.tsx
@@ -1,21 +1,35 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, Grid, Typography } from "@mui/material";
 import { Book } from "../types";
+import { Link } from "react-router-dom";
 
 const BookList = ({ books }: { books: Book[] }) => {
-  console.log("Books array:", books); 
-
-  return (
-    <div data-test="book-list">
-      {books.map((book, index) => {
-        console.log("Book ID:", book.id); 
-        return (
-          <div className="book-item" key={book.id || index}>
-            <h2 className="title">{book.name}</h2>
-            <a href={`/books/${book.id}`}>View Details</a>
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div data-test='book-list'>
+            <Grid container spacing={3}>
+                {books.map(book => (
+                    <Grid item xs={4} sm={4} key={book.id} className="book-item">
+                        <Card>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {book.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {book.description}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    <Link to={`/books/${book.id}`}>View Details</Link>
+                                </Button>
+                            </CardActions>
+                        </Card>                                                
+                    </Grid>
+                ))}
+            </Grid>        
+        </div>
+    );
 };
 
 export default BookList;
